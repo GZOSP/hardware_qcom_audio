bf6eaa53f67c1c16c21789002eb748c40a519a3c
Satish Babu Patakokila <sbpata@codeaurora.org>
2018-04-04 18:23:11 +0530

    hal: Fix compile errors in 32-bit for 8937
    - Fix compilation issues in 32bit for 8937
    
    Change-Id: Ia84a80dd115bc5a7bb1727f104b2dd2e13539777

--- hal/audio_hw.c
+++ hal/audio_hw.c
@@ -4520,7 +4520,7 @@ exit:
                             out_get_sample_rate(&out->stream.common));
 
         if (audio_extn_passthru_is_passthrough_stream(out)) {
-                //ALOGE("%s: write error, ret = %zd", __func__, ret);
+                ALOGE("%s: write error, ret = %d", __func__,(int) ret);
                 ATRACE_END();
                 return ret;
         }
